33397e11363abd66578f90a424986ae79d558c66
lil angel <cvndyperov@gmail.com>
2022-04-27 17:46:26 +0500

    base algo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,24 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class Main {
+
+    public static Integer findFoo(Map<Integer, Integer> dict, List<Integer> cache) {
+        Integer bestIndex = null;
+        Integer bestIndexCount = Integer.MAX_VALUE;
+        for (Integer key : cache) {
+            if (dict.get(key) < bestIndexCount) {
+                bestIndex = key;
+                bestIndexCount = dict.get(key);
+            }
+        }
+        return bestIndex;
+    }
 
 
     public static void main(String[] args) throws IOException {
@@ -30,5 +39,19 @@ public class Main {
                 dict.put(currentInt, dict.get(currentInt) + 1);
             }
         }
+        int count = 0;
+        List<Integer> cache = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            Integer currElem = queue.poll();
+            dict.put(currElem, dict.get(currElem) - 1);
+            if (!cache.contains(currElem)) {
+                count++;
+                if (cache.size() >= cacheSize) {
+                    cache.remove(findFoo(dict, cache));
+                }
+                cache.add(currElem);
+            }
+        }
+        System.out.println(count);
     }
 }
