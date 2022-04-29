afff5cb072c6b0bd5bd246008bdedf966c1a3fdf
lil angel <cvndyperov@gmail.com>
2022-04-29 14:26:10 +0500

    awesome working algo done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class Main {
 
-    public static Integer findFoo(Map<Integer, Integer> dict, List<Integer> cache) {
-        Integer bestIndex = null;
-        Integer bestIndexCount = Integer.MAX_VALUE;
-        for (Integer key : cache) {
-            if (dict.get(key) < bestIndexCount) {
-                bestIndex = key;
-                bestIndexCount = dict.get(key);
-            }
-        }
-        return bestIndex;
+    record Pair(Long left, Integer right) {
     }
-
 
     public static void main(String[] args) throws IOException {
         final String filePath = "src/main/resources/input.txt";
@@ -27,29 +15,49 @@ public class Main {
         String currentLine = reader.readLine();
         String[] nums = currentLine.split(" ");
         int cacheSize = Integer.parseInt(nums[0]);
-        Map<Integer, Integer> dict = new HashMap<>();
-        Queue<Integer> queue = new ArrayDeque<>();
+        Map<Long, TreeSet<Integer>> requestsFrequency = new HashMap<>();
+        Queue<Long> requestsQueue = new ArrayDeque<>();
         for (int i = 0; i < Integer.parseInt(nums[1]); i++) {
             currentLine = reader.readLine();
-            Integer currentInt = Integer.parseInt(currentLine);
-            queue.add(currentInt);
-            if (!dict.containsKey(currentInt)) {
-                dict.put(currentInt, 1);
+            Long currentLong = Long.parseLong(currentLine);
+            requestsQueue.add(currentLong);
+            if (!requestsFrequency.containsKey(currentLong)) {
+                TreeSet<Integer> ts = new TreeSet<>();
+                ts.add(i);
+                requestsFrequency.put(currentLong, ts);
             } else {
-                dict.put(currentInt, dict.get(currentInt) + 1);
+                requestsFrequency.get(currentLong).add(i);
             }
         }
         int count = 0;
-        List<Integer> cache = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            Integer currElem = queue.poll();
-            dict.put(currElem, dict.get(currElem) - 1);
-            if (!cache.contains(currElem)) {
-                count++;
-                if (cache.size() >= cacheSize) {
-                    cache.remove(findFoo(dict, cache));
-                }
-                cache.add(currElem);
+        SortedSet<Pair> cache = new TreeSet<>(new Comparator<Pair>() {
+            @Override
+            public int compare(Pair o1, Pair o2) {
+                return o1.right.compareTo(o2.right);
+            }
+        });
+        for (int i = 0; i < Integer.parseInt(nums[1]); i++) {
+            Long currentRequest = requestsQueue.poll();
+            var currentRequestNextPositions = requestsFrequency.get(currentRequest);
+            Pair currentPair = new Pair(currentRequest, currentRequestNextPositions.first());
+            currentRequestNextPositions.remove(currentRequestNextPositions.first());
+            if (currentRequestNextPositions.size() == 0) {
+                currentRequestNextPositions.add(100001);
+            }
+            if (cache.contains(currentPair)) {
+                cache.remove(currentPair);
+                currentPair = new Pair(currentRequest, currentRequestNextPositions.first());
+                cache.add(currentPair);
+                continue;
+            }
+            count++;
+            if (cache.size() < cacheSize) {
+                currentPair = new Pair(currentRequest, currentRequestNextPositions.first());
+                cache.add(currentPair);
+            } else {
+                cache.remove(cache.last());
+                currentPair = new Pair(currentRequest, currentRequestNextPositions.first());
+                cache.add(currentPair);
             }
         }
         System.out.println(count);
